f16cd716cd962fd4461a475fb0a960e6c3ab42df
HM <hieumdd@gmail.com>
2021-09-22 23:41:58 +0700

    feat: Updated table data & name & schema, Added sleep before navigate to report

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ CHROME_OPTIONS.add_experimental_option(
 
 BQ_CLIENT = bigquery.Client()
 DATASET = "SBLA_Yotpo"
-TABLE = "CampaignPerformance"
+TABLE = "TotalRevenueSMS"
 
 NOW = datetime.utcnow()
 
@@ -42,7 +42,6 @@ def get_csv_url():
     """
     if os.getenv("PYTHON_ENV") == "dev":
         driver = webdriver.Chrome("./chromedriver", options=CHROME_OPTIONS)
-        driver
     else:
         driver = webdriver.Chrome(options=CHROME_OPTIONS)
     driver.implicitly_wait(20)
@@ -65,6 +64,7 @@ def get_csv_url():
     login_button.click()
 
     # Navigate to Report
+    time.sleep(5)
     report_menu_item = driver.find_elements_by_xpath('//*[@id="menu"]/li[9]/a/span[2]')[
         0
     ]
@@ -125,20 +125,7 @@ def get_data(url):
     csv_lines = decoded_content.splitlines()
     cr = csv.DictReader(
         csv_lines[1:],
-        fieldnames=(
-            "Campaign Name",
-            "Scheduled date",
-            "Revenue",
-            "Cost",
-            "ROI",
-            "Sent Msgs",
-            "Clicks",
-            "CTR",
-            "CVR",
-            "Orders",
-            "AOV",
-            "Unsubs %",
-        ),
+        fieldnames=csv_lines[0].split(","),
     )
     return [row for row in cr]
 
@@ -153,25 +140,35 @@ def transform(rows):
         list: List of row transformed
     """
 
-    transform_currency = lambda x: float(re.sub(r"[^\d.]", "", x))
-    transform_percentage = lambda x: float(x.strip("%")) / 100 if x != "--" else None
+    transform_currency = (
+        lambda x: round(float(re.sub(r"[^\d.]", "", x)), 8) if x != "--" else None
+    )
+    transform_percentage = (
+        lambda x: round(float(x.strip("%")) / 100, 8) if x != "--" else None
+    )
+    transform_date = (
+        lambda x: datetime.strptime(x, "%d.%m.%Y").strftime("%Y-%m-%d")
+        if x != "--"
+        else None
+    )
     transform_datetime = lambda x: datetime.strptime(x, "%B %d %Y %H:%M").isoformat(
         timespec="seconds"
     )
     return [
         {
-            "campaign_name": row["Campaign Name"],
-            "scheduled_date": transform_datetime(row["Scheduled date"]),
-            "revenue": transform_currency(row["Revenue"]),
-            "cost": transform_currency(row["Cost"]),
-            "roi": transform_percentage(row["ROI"]),
-            "sent_msgs": int(row["Sent Msgs"]),
-            "clicks": int(row["Clicks"]),
-            "ctr": transform_percentage(row["CTR"]),
-            "cvr": transform_percentage(row["CVR"]),
-            "orders": int(row["Orders"]),
-            "aov": transform_currency(row["AOV"]),
-            "unsubs_rate": transform_percentage(row["Unsubs %"]),
+            "period": transform_date(row["Period"]),
+            "total_cost": transform_currency(row["Total Cost"]),
+            "total_revenue": transform_currency(row["Total Revenue"]),
+            "total_roi": transform_percentage(row["Total ROI"]),
+            "campaigns_cost": transform_currency(row["Campaigns Cost"]),
+            "campaigns_revenue": transform_currency(row["Campaigns Revenue"]),
+            "campaigns_roi": transform_percentage(row["Campaigns ROI"]),
+            "flows_cost": transform_currency(row["Flows Cost"]),
+            "flows_revenue": transform_currency(row["Flows Revenue"]),
+            "flows_roi": transform_percentage(row["Flows ROI"]),
+            "automations_cost": transform_currency(row["Automations Cost"]),
+            "automations_revenue": transform_currency(row["Automations Revenue"]),
+            "automations_roi": transform_percentage(row["Automations ROI"]),
             "_batched_at": NOW.isoformat(timespec="seconds"),
         }
         for row in rows
@@ -187,8 +184,7 @@ def load(rows):
     Returns:
         int: Output rows
     """
-
-    return (
+    output_rows = (
         BQ_CLIENT.load_table_from_json(
             rows,
             f"{DATASET}._stage_{TABLE}",
@@ -196,18 +192,19 @@ def load(rows):
                 create_disposition="CREATE_IF_NEEDED",
                 write_disposition="WRITE_APPEND",
                 schema=[
-                    {"name": "campaign_name", "type": "STRING"},
-                    {"name": "scheduled_date", "type": "DATETIME"},
-                    {"name": "revenue", "type": "FLOAT"},
-                    {"name": "cost", "type": "FLOAT"},
-                    {"name": "roi", "type": "FLOAT"},
-                    {"name": "sent_msgs", "type": "INTEGER"},
-                    {"name": "clicks", "type": "STRING"},
-                    {"name": "ctr", "type": "FLOAT"},
-                    {"name": "cvr", "type": "FLOAT"},
-                    {"name": "orders", "type": "INTEGER"},
-                    {"name": "aov", "type": "FLOAT"},
-                    {"name": "unsubs_rate", "type": "FLOAT"},
+                    {"name": "period", "type": "DATE"},
+                    {"name": "total_cost", "type": "NUMERIC"},
+                    {"name": "total_revenue", "type": "NUMERIC"},
+                    {"name": "total_roi", "type": "NUMERIC"},
+                    {"name": "campaigns_cost", "type": "NUMERIC"},
+                    {"name": "campaigns_revenue", "type": "NUMERIC"},
+                    {"name": "campaigns_roi", "type": "NUMERIC"},
+                    {"name": "flows_cost", "type": "NUMERIC"},
+                    {"name": "flows_revenue", "type": "NUMERIC"},
+                    {"name": "flows_roi", "type": "NUMERIC"},
+                    {"name": "automations_cost", "type": "NUMERIC"},
+                    {"name": "automations_revenue", "type": "NUMERIC"},
+                    {"name": "automations_roi", "type": "NUMERIC"},
                     {"name": "_batched_at", "type": "TIMESTAMP"},
                 ],
             ),
@@ -215,18 +212,20 @@ def load(rows):
         .result()
         .output_rows
     )
+    update()
+    return output_rows
 
 
 def update():
     """Update the main table"""
 
     query = f"""
-    CREATE OR REPLACE TABLB {DATASET}.{TABLE} AS
+    CREATE OR REPLACE TABLE {DATASET}.{TABLE} AS
     SELECT * EXCEPT (row_num)
     FROM (
         SELECT
             *,
-            ROW_NUMBER() OVER (PARTITION BY campaign_name, scheduled_date ORDER BY _batched_at)
+            ROW_NUMBER() OVER (PARTITION BY period ORDER BY _batched_at)
             AS row_num
         FROM {DATASET}._stage_{TABLE}
     ) WHERE row_num = 1"""
